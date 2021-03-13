d97c152a1d2708b33c3fd6a90561267318275d4b
anhao12138 <1993697020@qq.com>
2021-03-13 16:37:50 +0800

    master

---
+++ src/main/java/com/example/online_re/service/ImageAdsService.java
@@ -0,0 +1,9 @@
+package com.example.online_re.service;
+
+/**
+ * @Date 2021/3/13 16:33
+ * @Version 1.0
+ */
+
+public interface ImageAdsService {
+}
---
+++ src/main/java/com/example/online_re/service/impl/ImageAdsServiceImpl.java
@@ -0,0 +1,9 @@
+package com.example.online_re.service.impl;
+
+/**
+ * @Date 2021/3/13 16:35
+ * @Version 1.0
+ */
+
+public class ImageAdsServiceImpl {
+}
